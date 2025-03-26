9cd851b19900338337e712b729db7715f13a94db
Sean Tichenor <jtart2133014@gmail.com>
2025-03-26 15:40:28 -0400

    Update launch_protected.sh

--- bin/launch_protected.sh
+++ bin/launch_protected.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+
+if [[ "$OSTYPE" == "android"* || "$TERMUX_VERSION" != "" ]]; then
+    export DOMINANCEX_MODE="MOBILE"
+    export TGDK_HOME="$HOME/Makeshift"
+    echo "[DominanceX] Mobile mode engaged via Termux or shell emulator"
+fi
+
 export LD_PRELOAD=$HOME/.dominancex/bin/libdominancex.so
 exec "$@"
